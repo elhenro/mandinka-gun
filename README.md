a7d36a275a70c7bbd4acc1846f3c0c6911cf1414
elhenro <github@henry-schober.com>
2017-12-18 18:55:40 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# simple-list-gun-server
+## data.json
+where the gun data is stored
+## gun-server.js
+Runs the GUN Server, can be run locally or also be deploy f.e. to Heroku
+## test.html
+Test-Preview of the Live-Synched List
---
+++ gun-server.js
@@ -0,0 +1,35 @@
+/*
+// import the native http module
+var http = require('http')
+
+// import gun
+var Gun = require('gun')
+
+// create a new gun instance
+var gun = new Gun()
+
+// create a new server instance
+var server = new http.Server()
+
+// add the /gun.js route to your server
+server.on('request', gun.wsp.server)
+
+/*
+  Handle incoming gun traffic
+  from clients (that's where the
+  real-time goodness comes from).
+*/
+/*
+gun.wsp(server)
+
+// start listening for requests on `localhost:8080`
+server.listen(8080)
+*/
+
+// import the server function
+var startGunServer = require('gun-server')
+
+// use said function
+var gun = startGunServer()
+
+// by default, it'll listen on http://localhost:8080
